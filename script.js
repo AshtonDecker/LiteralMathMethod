d274bbc1d96ea39d9e400bdc4a76d4b8f84a10bb
Ashton <ashton.decker@stu.barren.kyschools.us>
2024-02-01 11:03:57 -0600

    Exercise completed

---
+++ script.js
@@ -0,0 +1,20 @@
+let warmHugs = "Hi, I'm Olaf and I like warm hugs."
+// console.log(warmHugs);
+console.log(warmHugs.toUpperCase());
+warmHugs = "Hi, I'm Olaf and I like warm hugs."
+console.log(warmHugs.replace("like", "love"));
+warmHugs = "Hi, I'm Olaf and I love warm hugs."
+console.log(warmHugs);
+beenImpaled = "Oh, look at that. I've been impaled."
+trim1 = "Oh, look at that."
+trim2 =  "I've been impaled."
+console.log(trim2.trim("Oh, look at that."));
+dotDotDot = "..."
+skullBones = "I don't have a skull" + dotDotDot + "or bones."
+console.log(skullBones);
+console.log(Math.PI);
+randomNumber = Math.random();
+randomNumber = randomNumber * 3
+randomNumber = Math.floor(randomNumber);
+randomNumber++;
+console.log(randomNumber);
